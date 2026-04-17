c269d163e3be4f6a6a8f3e17fb03c4152d146e5f
Ram <163444056+chilliflake1313@users.noreply.github.com>
2026-04-17 12:17:41 +0530

    Add doodle bookmarks extension foundation

---
+++ doodle-bookmarks-extension/popup.js
@@ -0,0 +1,8 @@
+// Part 1: Basic initialization
+// Full implementation in Part 4
+
+document.addEventListener('DOMContentLoaded', () => {
+	console.log('Doodle Bookmarks Extension Loaded');
+  
+	// Main initialization will be in Part 4
+});
---
+++ doodle-bookmarks-extension/utils.js
@@ -0,0 +1,10 @@
+// Part 1: Basic utility file structure
+// Full implementation in Part 3
+
+const StorageUtils = {
+	// To be implemented in Part 3
+};
+
+const DataUtils = {
+	// To be implemented in Part 3
+};
